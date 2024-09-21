c48e32da98108eedb6daf74f72f3a8ce146d53a4
Nick-stack517 <gsatello@gmail.com>
2024-09-22 04:35:34 +0700

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,10 @@
+my_string = input('Password: ')
+
+print('Количество символов: ',len(my_string))
+
+print('Верхний регистр:     ',my_string.upper())
+print('Нижний регистр:      ',my_string.lower())
+print("Без пробелов:        ",my_string.replace(' ',''))
+print('Первый символ:       ',my_string[0])
+print('Последний символ:    ',my_string[-1])
+
